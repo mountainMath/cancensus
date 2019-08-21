c3dc80f437291ed5db834e8dcc0ea5d60427aa40
Jens von Bergmann <jens@mountainmath.ca>
2019-08-21 10:40:03 -0700

    Fix vignette with toronto plot

--- doc/Making_maps_with_cancensus.R
+++ doc/Making_maps_with_cancensus.R
@@ -17,8 +17,8 @@ toronto <- get_census(dataset='CA16', regions=list(CMA="35535"),
 plot(toronto["median_hh_income"], main = "Toronto Household Income by CSD")
 
 ## ------------------------------------------------------------------------
-plot(toronto[14], col = NA, main = "Toronto CSDs with Median HH Income > 100,000", lty = 3)
-plot(toronto[toronto$median_hh_income > 100000,14], col = "red", add = TRUE)
+plot(st_geometry(toronto), col = NA, main = "Toronto CSDs with Median HH Income > $100,000", lty = 3)
+plot(st_geometry(toronto[toronto$median_hh_income > 100000,]), col = "red", add = TRUE)
 
 ## ------------------------------------------------------------------------
 library(ggplot2)
@@ -35,7 +35,8 @@ ggplot(toronto) + geom_sf(aes(fill = median_hh_income), colour = "grey") +
 
 ## ----message=FALSE, warning=FALSE, include=FALSE-------------------------
 # This chunk disables leaflet output in remaining chunks for PDF vignettes
-if(Sys.getenv("COMPILE_VIG")==FALSE) {knitr::opts_chunk$set(eval = FALSE)}
+if(Sys.getenv("COMPILE_VIG")!="TRUE") {
+  knitr::opts_chunk$set(eval = FALSE)}
 
 ## ---- message = FALSE----------------------------------------------------
 library(leaflet)
@@ -45,7 +46,7 @@ leaflet(toronto) %>%
 
 ## ------------------------------------------------------------------------
 bins <- c(0, 30000,40000, 50000,60000, 70000,80000, 90000,100000, 110000, Inf)
-pal <- colorBin("RdYlBu", domain = toronto$median_hh_income, bins = bins)
+pal <- colorBin("RdYlBu", domain = toronto$v_CA16_2397, bins = bins)
 leaflet(toronto) %>% 
   addProviderTiles(providers$CartoDB.Positron) %>%
   addPolygons(fillColor = ~pal(median_hh_income),
