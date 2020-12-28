32791e8d169737fa8a5ffb8ec093d4d35714f737
Jens von Bergmann <jens@mountainmath.ca>
2020-12-27 22:21:04 -0800

    fix issue with cache directory

--- R/helpers.R
+++ R/helpers.R
@@ -15,8 +15,9 @@ robust_api_key <- function(api_key){
 # Append arguments to the path of the local cache directory.
 cache_path <- function(...) {
   cache_dir <- Sys.getenv("CM_CACHE_PATH")
-  if (nchar(cache_dir)==0) cache_dir <- getOption("cancensus.cache_path")
-  if (is.null(cache_dir) | nchar(cache_dir)==0) cache_dir <- tempdir()
+  if (nchar(cache_dir)==0 & !is.null(getOption("cancensus.cache_path"))) {
+    cache_dir <- getOption("cancensus.cache_path")
+  } else cache_dir <- tempdir()
   if (!is.character(cache_dir)) {
     stop("Corrupt 'CM_CACHE_PATH' environment variable or 'cancensus.cache_path' option. Must be a path.",
          .call = FALSE)
