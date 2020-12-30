746c8ba13fb60b6a902a0cc36486e70890f8b345
Jens von Bergmann <jens@mountainmath.ca>
2020-12-29 23:13:19 -0800

    try moving manual crs setting to dataset

--- data-raw/COV_SKYTRAIN_STATIONS.R
+++ data-raw/COV_SKYTRAIN_STATIONS.R
@@ -4,6 +4,7 @@
 COV_SKYTRAIN_STATIONS <- VancouvR::get_cov_data("rapid-transit-stations",format="geojson") %>%
   st_transform(26910) %>%
   st_buffer(800) %>%
-  st_transform(4326)
+  st_transform(4326) %>%
+  st_set_crs(4326) # needed for ubuntu with old GDAL
 
 usethis::use_data(COV_SKYTRAIN_STATIONS, overwrite = TRUE)
