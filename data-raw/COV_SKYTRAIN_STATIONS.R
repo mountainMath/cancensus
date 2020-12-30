f3020d2eec4f6ba136bf13c53dc278f9a48d8332
Jens von Bergmann <jens@mountainmath.ca>
2020-12-29 23:22:43 -0800

    try moving skytrain station data handlink back into the vignette

--- data-raw/COV_SKYTRAIN_STATIONS.R
+++ data-raw/COV_SKYTRAIN_STATIONS.R
@@ -1,10 +1,6 @@
 ## code to prepare `COV_SKYTRAIN_STATIONS` dataset goes here
 
 
-COV_SKYTRAIN_STATIONS <- VancouvR::get_cov_data("rapid-transit-stations",format="geojson") %>%
-  st_transform(26910) %>%
-  st_buffer(800) %>%
-  st_transform(4326) %>%
-  st_set_crs(4326) # needed for ubuntu with old GDAL
+COV_SKYTRAIN_STATIONS <- VancouvR::get_cov_data("rapid-transit-stations",format="geojson")
 
 usethis::use_data(COV_SKYTRAIN_STATIONS, overwrite = TRUE)
