a411a13f4d417dc0aac3cae33ec04f0fc16f88b9
Jens von Bergmann <jens@mountainmath.ca>
2020-12-29 23:29:55 -0800

    go back to transform/buffer in the data set creation and then set crs manually in vignette.

--- data-raw/COV_SKYTRAIN_STATIONS.R
+++ data-raw/COV_SKYTRAIN_STATIONS.R
@@ -1,6 +1,9 @@
 ## code to prepare `COV_SKYTRAIN_STATIONS` dataset goes here
 
 
-COV_SKYTRAIN_STATIONS <- VancouvR::get_cov_data("rapid-transit-stations",format="geojson")
+COV_SKYTRAIN_STATIONS <- VancouvR::get_cov_data("rapid-transit-stations",format="geojson") %>%
+  st_transform(26910) %>%
+  st_buffer(800) %>%
+  st_transform(4326)
 
 usethis::use_data(COV_SKYTRAIN_STATIONS, overwrite = TRUE)
